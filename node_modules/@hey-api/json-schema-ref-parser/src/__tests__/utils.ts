c2685c89cad236028c022a39e70a64f0d978e055
valenn115 <67602412+valenn115@users.noreply.github.com>
2026-06-18 14:11:33 +0300

    chore: regenerate from 9eeeeed

--- node_modules/@hey-api/json-schema-ref-parser/src/__tests__/utils.ts
+++
@@ -1,3 +0,0 @@
-import path from 'node:path';
-
-export const getSpecsPath = (): string => path.join(__dirname, '..', '..', '..', '..', 'specs');
--- node_modules/@hey-api/json-schema-ref-parser/src/util/convert-path-to-posix.ts
+++
@@ -1,8 +0,0 @@
-export default function convertPathToPosix(filePath: string): string {
-  // Extended-length paths on Windows should not be converted
-  if (filePath.startsWith('\\\\?\\')) {
-    return filePath;
-  }
-
-  return filePath.replaceAll('\\', '/');
-}
